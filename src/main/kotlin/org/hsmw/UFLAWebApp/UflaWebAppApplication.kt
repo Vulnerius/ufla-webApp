9fc5818df63b73663248d284d6203eeb0f78ffc3
vulnerius <pfuchs480@gmail.com>
2022-10-25 09:58:19 +0200

    :tada: project init

---
+++ src/main/kotlin/org/hsmw/UFLAWebApp/UflaWebAppApplication.kt
@@ -0,0 +1,11 @@
+package org.hsmw.UFLAWebApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UflaWebAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<UflaWebAppApplication>(*args)
+}
